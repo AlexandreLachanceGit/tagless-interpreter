ea9b02e7fb0993364243ea2ccb135dac018f6ffc
Lachance, Alexandre <alexandrelachance@me.com>
2024-03-10 14:25:17 -0400

    Redo main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
-module Main where
+module Main (main) where
 
 import qualified Data.Text.IO as TIO
 
@@ -10,20 +10,9 @@ import HaskellRepInterpreter
 import LengthInterpreter
 import PrettyPrintInterpreter
 
--- Testing
+-- Using example given in paper just to showcase capabilities
 
-td1 = mult (add (int 1) (int 2)) (minus (int 3))
-
-td2 = and_ (bool True) (bool False)
-td3 = not_ (or_ (bool True) (bool False))
-
-td4 = and_ (gte (int 2) (add (int 1) (int 1))) (lt (minus (int 1)) (int 1))
-
-td5 = if_ (eq (int 2) (int 2)) (int 1) (int 2)
-
-td6 = second (pair (add (int 14) (int 2), bool False))
-
-
+tpow :: Symantics repr => repr (Int -> Int -> Int)
 tpow =
     lam
         ( \x ->
@@ -39,27 +28,19 @@ tpow =
                 )
         )
 
+tpow7 :: Symantics repr => repr (Int -> Int)
 tpow7 = lam (\x -> app (app tpow x) (int 7))
-tpow72 = app tpow7 (int 10)
+
+tpow7_10 :: Symantics repr => repr Int
+tpow7_10 = app tpow7 (int 10)
 
 main :: IO ()
 main = do
-       print (eval td1)
-       print (eval td2)
-       print (eval td3)
-       print (eval td4)
-       print (eval td5)
-       print (eval (add ((mult (minus (int 4)) (int 4))) (int 2)))
-       print (eval (first (pair (add (int 1) (int 3), int 2))))
-       print (len td1)
-       print (len (app (lam (\x -> (add x (int 1)))) (int 1)))
-       print (len tpow72)
-       print (len td6)
-       TIO.putStrLn (haskellView td6)
-       TIO.putStrLn (haskellView td4)
-       TIO.putStrLn (haskellView tpow72)
-       TIO.putStrLn (haskellView tpow)
-       TIO.putStrLn (prettyView td6)
-       TIO.putStrLn (prettyView td4)
-       TIO.putStrLn (prettyView tpow72)
-       TIO.putStrLn (prettyView tpow)
+       putStrLn "Program pretty view:"
+       TIO.putStrLn (prettyView tpow7_10)
+       putStrLn "Program valid haskell representation:"
+       TIO.putStrLn (haskellView tpow7_10)
+       putStrLn "Length of program:"
+       print (len tpow7_10)
+       putStrLn "Program output:"
+       print (eval tpow7_10)
