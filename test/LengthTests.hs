a024ba7ebd1137b4860b17b0fc77d5f8d995144b
Lachance, Alexandre <alexandrelachance@me.com>
2024-03-10 10:59:54 -0400

    Length tests

--- test/LengthTests.hs
+++ test/LengthTests.hs
@@ -11,10 +11,30 @@ import PrettyPrintInterpreter
 
 -- Test programs
 
-simpleAndFalse = and_ (bool True) (bool False)
+oneLenProg = (int 10)
+simpleProg = and_ (bool True) (eq (int 5) (add (int 3) (int 2)))
+
+tpow = lam
+        ( \x ->
+            fix
+                ( \self ->
+                    lam
+                        ( \n ->
+                            if_
+                                (lte n (int 0))
+                                (int 1)
+                                (mult x (app self (add n (int (-1)))))
+                        )
+                )
+        )
+
+tpow7 = lam (\x -> app (app tpow x) (int 7))
+complexProg = app tpow7 (int 10)
 
 -- Tests: Program length
 tests = [
-    TestCase (assertEqual "True && False" (len simpleAndFalse) 3)
+    TestCase (assertEqual "One Length Program" (len oneLenProg) 1),
+    TestCase (assertEqual "Simple Program" (len simpleProg) 7),
+    TestCase (assertEqual "Complex Program" (len complexProg) 17)
     ]
 
